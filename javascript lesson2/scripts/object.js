2257c68ab3c09ed0e8c77fb2c78a2abe0f3a6efc
GiorgiNikolaishvilii <nikolaishvili.giorgi22@gtu.ge>
2023-11-02 22:13:37 +0400

    Update
    Adding update.

---
+++ javascript lesson2/scripts/object.js
@@ -0,0 +1,21 @@
+let person ={
+    name: "John",
+    age:30,
+    isMarried:true,
+    childrean:[
+        {
+            name:"Luke",
+            age: 5
+        },
+        {
+            name:"George",
+            age: 3
+        },
+        {
+            name:"marry",
+            age: 1
+        }
+    ]
+};
+
+console.log(person.childrean[0].name);
